7398e0eeb05263884c510186bfe2d391133c580d
milana <azimbaeva.milana2004@gmail.com>
2025-02-26 10:06:48 +0600

    second

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,52 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+document.addEventListener("scroll", function () {
+    let contactTitle = document.querySelector(".contact-title");
+    let scrollY = window.scrollY;
+    let fadePoint = 150; 
+
+    if (scrollY < fadePoint) {
+        contactTitle.style.opacity = 1 - scrollY / fadePoint;
+    } else {
+        contactTitle.style.opacity = 0;
+    }
+});
+
+
+
+
+function goToPage(pageNumber) {
+    const links = document.querySelectorAll('.pagination a');
+    links.forEach(link => link.classList.remove('active'));  
+  
+    const pageLink = Array.from(links).find(link => link.innerText == pageNumber);
+    pageLink.classList.add('active');
+
+    console.log("Loading content for page:", pageNumber);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const navbarToggle = document.querySelector('.navbar-toggle');
+    const navbarOptions = document.querySelector('.navbar-options');
+
+    if (navbarToggle && navbarOptions) {
+        navbarToggle.addEventListener('click', function () {
+            navbarOptions.classList.toggle('active');
+        });
+    }
+});
